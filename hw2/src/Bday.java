d834d7cae5ef510c04b68318ee4f181eb1eed2bb
Rubin Peci <rubinpeci@gmail.com>
2019-09-23 14:14:53 -0400

    Complete mm=0 case for Paradox and finish written assignment

--- hw2/src/Bday.java
+++ hw2/src/Bday.java
@@ -2,48 +2,50 @@ import java.util.Random;
 
 class Bday {
 	// MEMBERS:
-		int day;
+		int day;	//stores the random int that determines what day the birthday is
 	// CONSTRUCTOR:
 			Bday (Random rg){
 				day = rg.nextInt(365) + 1;
 			}
 	// METHODS:
+	//Returns an int array in the format (month, day)
+	//Does various logic tests on the number provided, n, along with some math to adjust it for MM-DD format
 		int[] convert(int n){
 			int[] pair = new int[2]; 
-			if (n <= 31) {
+			if (n <= 31) {				//jan
 				pair[0] = 1;
 				pair[1] = n;
-			} else if (n <= 59) {
+			} else if (n <= 59) {		//feb
 				pair[0] = 2;
 				pair[1] = n - 31;
-			} else if (n <= 90) {
+			} else if (n <= 90) {		//mar
 				pair[0] = 3;
 				pair[1] = n - 59;
-			} else if (n <= 120) {
+			} else if (n <= 120) {		//apr
 				pair[0] = 4;
 				pair[1] = n - 90;
-			} else if (n <= 151) {
+			} else if (n <= 151) {		//may
 				pair[0] = 5;
 				pair[1] = n - 120;
-			} else if (n <= 181) {
+			} else if (n <= 181) {		//june
 				pair[0] = 6;
 				pair[1] = n - 151;
-			} else if (n <= 212) {
+			} else if (n <= 212) {		//july
 				pair[0] = 7;
 				pair[1] = n - 181;;
-			} else if(n <= 243) {
+			} else if(n <= 243) {		//aug
 				pair[0] = 8;
 				pair[1] = n - 212;
-			} else if (n <= 273) {
+			} else if (n <= 273) {		//sep
 				pair[0] = 9;
 				pair[1] = n - 243;
-			} else if (n <= 304) {
+			} else if (n <= 304) {		//oct
 				pair[0] = 10;
-				pair[1] = n - 273;
-			} else if (n <= 334) {
+				pair[1] = n - 273;	
+			} else if (n <= 334) {		//nov
 				pair[0] = 11;
-				pair[1] = n - 304;
-			} else if (n <= 365) {
+				pair[1] = n - 304;	
+			} else if (n <= 365) {		//dec
 				pair[0] = 12;
 				pair[1] = n - 334;
 			}
@@ -51,6 +53,7 @@ class Bday {
 		}
 		public String toString(){
 			int[] pair = convert(day);
+			//run convert on the value of day and check the values provided to see what month to print out
 			switch(pair[0]) {
 				case 1:
 					return "Jan " + pair[1];
@@ -81,6 +84,6 @@ class Bday {
 		}
 
 		boolean isEqual(Bday bd) {
-			return bd.day == this.day;
+			return bd.day == this.day;	//simply compare values of the day number
 		}
 }//Bday class
--- hw2/src/Paradox.java
+++ hw2/src/Paradox.java
@@ -7,24 +7,46 @@ class Paradox {
 	
 	// METHOD:========================================
 		static boolean test(int v){
+			Bday[] bdays = new Bday[nn];
+			for(int i = 0; i < bdays.length; i++) {
+				bdays[i] = new Bday(rg);
+			}
+			if (v == 1) {
+				for(int i = 0; i < bdays.length; i++) {
+					for(int n = 0; n < bdays.length; n++) {
+						if (bdays[i].isEqual(bdays[n]) && n != i) {
+							System.out.println("Bday1: " + bdays[i] + "\nBday2: " + bdays[n]);
+							return true;
+						}
+					}
+				}
+				for(Bday b : bdays)
+					System.out.println(b);
+				return false;
+			} else if (v == 0) {
+
+			}
 			return false;
 		}
 		static String showArgs(){
 			// Use String.format to show the values of ss, nn, mm, pp
-			return "Args:";
+			return String.format("Args: ss = %d, nn = %d, mm = %d, pp = %d", ss, nn, mm, pp);
 		}
 	// MAIN METHOD:===================================
 		public static void main(String[] args){
-				ss=111;
-				nn=24;
-				mm=0;
-				pp=100;
 			// Next, override these values with command line arguments for
 			//			ss, nn, mm, pp
 			// in this order!
 			
+			ss = args.length == 4 ? Integer.parseInt(args[0]) : 111;
+			nn = args.length == 4 ? Integer.parseInt(args[1]) : 24;
+			mm = args.length == 4 ? Integer.parseInt(args[2]) : 0;
+			pp = args.length == 4 ? Integer.parseInt(args[3]) : 100;
+			System.out.println(showArgs());
+
+			
 			// YOU MUST use the "seed" for rg in this way:
-				rg = (ss==0)? new Random() : new Random(ss);
+			rg = (ss==0) ? new Random() : new Random(ss);
 
 			switch(mm){
 				case 0:

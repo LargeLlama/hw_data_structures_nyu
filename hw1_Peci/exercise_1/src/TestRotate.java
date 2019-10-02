3c881f1c31c626fcd2376cb1aaf9bae42f77fa3c
Rubin Peci <rubinpeci@gmail.com>
2019-10-02 17:50:18 -0400

    Fixes

--- hw1_Peci/exercise_1/src/TestRotate.java
+++ hw1_Peci/exercise_1/src/TestRotate.java
@@ -1,7 +1,7 @@
 public class TestRotate {
 
 	public static void main (String args[]) {
-		//Create a 2 by 5 Rectangle
+	/*	//Create a 2 by 5 Rectangle
 		Rectangle rect1 = new Rectangle(2.0, 5.0);
 		System.out.println("Rectangle 1: " + rect1);
 		//Testing out NonDestRotate()
@@ -23,7 +23,17 @@ public class TestRotate {
 		//Testing out DestRotate()
 		System.out.println("Testing out DestRotate()");
 		System.out.println("Rotated (destructively) lrect1: " + lrect1.DestRotate());
-		System.out.println("Rectangle 1: " + lrect1);
+		System.out.println("Rectangle 1: " + lrect1); */
+		Rectangle rr = new LocatedRect(1,2,3,4);
+		// System.out.println(rr.top()); // ERROR!!
+		if (rr instanceof Rectangle) 
+			System.out.println("I am rect");
+		else 
+			System.out.println("I am no rect!");
 
+		if (rr instanceof LocatedRect) 
+			System.out.println("I am locatedrect");
+		else 
+			System.out.println("I am no LocatedRect");
 	}
 }
